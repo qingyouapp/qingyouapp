00ad639c646c6496ee0ea00d73933762c0f36573
xiayong <xiayong@192.168.1.102>
2014-12-02 23:13:22 +0800

    11

--- src/com/xwq/qingyouapp/MainActivity.java
+++ src/com/xwq/qingyouapp/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends Activity {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
+		//11.12
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
 			return true;
